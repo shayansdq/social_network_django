638697dfdcae08edc587939e87da3b89b7dd68a2
Shayan <shayansadeghi178@gmail.com>
2022-02-12 19:13:09 +0330

    create homeview class

--- home/views.py
+++ home/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import View
 
 
 # Create your views here.
 
-def home(request):
-    return HttpResponse('hello from the home')
+class HomeView(View):
+    def get(self, request):
+        return render(request, 'home/index.html')
+
+    

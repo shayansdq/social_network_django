702be0f7258f1fa255da3da13b077ba7282687db
Shayan <shayansadeghi178@gmail.com>
2022-02-12 20:21:07 +0330

    refactor and update add widgets

--- account/forms.py
+++ account/forms.py
@@ -2,6 +2,16 @@ from django import forms
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField()
-    email = forms.EmailField()
-    password = forms.CharField()
+    username = forms.CharField(widget=forms.TextInput(
+            attrs={'placeholder':'Enter your Username','class':'form-control'}
+        ))
+    email = forms.EmailField(
+        widget=forms.EmailInput(
+            attrs={'placeholder':'Enter your email','class':'form-control'}
+        )
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(
+            attrs={'placeholder':'Enter your password','class':'form-control'}
+        )
+    )

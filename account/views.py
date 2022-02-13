6eeb4fa07af7d2fc788392cc57ac9b3211206ef0
Shayan <shayansadeghi178@gmail.com>
2022-02-13 16:14:02 +0330

    write our views and use dispatch and setup functions in CBVs

--- account/views.py
+++ account/views.py
@@ -1,17 +1,89 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render, redirect
 from django.views import View
-from .forms import UserRegistrationForm
+from account.forms import UserRegistrationForm, UserLoginForm
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from home.models import Post
 
 
-# Create your views here.
+class UserRegisterView(View):
+    """register user view"""
+    template_name = 'account/register.html'
+    form_class = UserRegistrationForm
 
-class RegisterView(View):
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.error(request, 'login first!', 'warning')
+            return redirect('home:home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
     def get(self, request):
-        form = UserRegistrationForm()
-        context = {
-            'form':form
-        }
-        return render(request, 'account/register.html',context)
 
-    def post(self):
-        pass
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            User.objects.create_user(cd['username'], cd['email'], cd['password'])
+            messages.success(request, 'Registered successfully ! ', 'success')
+            return redirect('home:home')
+
+        # Handle Invalid form
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLoginView(View):
+    """login user view"""
+    template_name = 'account/login.html'
+    form_class = UserLoginForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            messages.error(request, 'login first!', 'warning')
+            return redirect('home:home')
+        else:
+            return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        form = self.form_class
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+            user = authenticate(request, username=cd['username'], password=cd['password'])
+            if user is not None:
+                login(request, user)
+                messages.success(request, 'logged in', 'success')
+                return redirect('home:home')
+            # Handle invalid username or password
+            messages.error(request, 'username or password is incorrect', 'warning')
+
+        # Handle Invalid form
+        return render(request, self.template_name, {'form': form})
+
+
+class UserLogoutView(LoginRequiredMixin, View):
+    """user logout view"""
+
+    def get(self, request):
+        logout(request)
+        messages.success(request, 'logged out', 'success')
+        return redirect('home:home')
+
+
+class UserProfileView(LoginRequiredMixin, View):
+    def get(self, request, user_id):
+        user = User.objects.get(pk=user_id)
+        posts = Post.objects.filter(user=user)
+        context = {
+            'user':user,
+            'posts':posts
+        }
+        return render(request, 'account/profile.html',context)
--- home/views.py
+++ home/views.py
@@ -1,12 +1,94 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import View
+from .models import Post
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from .forms import PostCreateUpdateForm
+from django.utils.text import slugify
 
 
 # Create your views here.
 
 class HomeView(View):
     def get(self, request):
-        return render(request, 'home/index.html')
+        posts = Post.objects.all()
+        context = {
+            'posts': posts
+        }
+        return render(request, 'home/index.html', context)
 
-    
+
+class PostDetailView(View):
+    def get(self, request, post_id, post_slug):
+        post = Post.objects.get(pk=post_id, slug=post_slug)
+        context = {
+            'post': post
+        }
+        return render(request, 'home/detail.html', context)
+
+
+class PostDeleteView(LoginRequiredMixin, View):
+    def get(self, request, post_id):
+        post = Post.objects.get(pk=post_id)
+        if post.user.id == request.user.id:
+            post.delete()
+            messages.success(request, 'post deleted successfully', 'success')
+        else:
+            messages.error(request, 'you cant delete this post', 'danger')
+        return redirect('home:home')
+
+
+class PostUpdateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def setup(self, request, *args, **kwargs):
+        self.post_instance = Post.objects.get(pk=kwargs['post_id'])
+        super().setup(request, *args, **kwargs)
+
+    def dispatch(self, request, *args, **kwargs):
+        post = self.post_instance
+        if not post.user.id == request.user.id:
+            messages.error(request, 'You cant update this post', 'danger')
+            return redirect('home:home')
+        return super().dispatch(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(instance=post)
+        context = {
+            'form': form
+        }
+        return render(request, 'home/update.html', context)
+
+    def post(self, request, *args, **kwargs):
+        post = self.post_instance
+        form = self.form_class(request.POST, instance=post)
+        if form.is_valid():
+            updated_post = form.save(commit=False)
+            updated_post.slug = slugify(form.cleaned_data['body'][:30])
+            updated_post.save()
+            messages.success(request, 'Your post was updated successfully', 'success')
+            return redirect('home:post_detail', post.id, post.slug)
+
+
+class PostCreateView(LoginRequiredMixin, View):
+    form_class = PostCreateUpdateForm
+
+    def get(self, request, *args, **kwargs):
+        form = self.form_class
+        context = {
+            'form':form
+        }
+        return render(request, 'home/create.html',context)
+
+    def post(self, request, *args, **kwargs):
+        form = self.form_class(request.POST)
+        if form.is_valid():
+            new_post = form.save(commit=False)
+            new_post.slug = slugify(form.cleaned_data['body'][:30])
+            new_post.user = request.user
+            new_post.save()
+            messages.success(request, 'Your post was created successfully', 'success')
+            redirect('home:post_detail',new_post.id,new_post.slug)
+

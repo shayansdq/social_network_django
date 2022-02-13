f1a603fd9baa94f6380a606abc091295b59f2220
Shayan <shayansadeghi178@gmail.com>
2022-02-13 16:10:50 +0330

    refactor & add an option for validating email and username

--- account/forms.py
+++ account/forms.py
@@ -1,17 +1,56 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
 
 class UserRegistrationForm(forms.Form):
     username = forms.CharField(widget=forms.TextInput(
-            attrs={'placeholder':'Enter your Username','class':'form-control'}
-        ))
+        attrs={'placeholder': 'Enter your Username', 'class': 'form-control'}
+    ))
     email = forms.EmailField(
         widget=forms.EmailInput(
-            attrs={'placeholder':'Enter your email','class':'form-control'}
+            attrs={'placeholder': 'Enter your email', 'class': 'form-control'}
         )
     )
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={'placeholder':'Enter your password','class':'form-control'}
-        )
-    )
+    password1 = forms.CharField(label='Password',
+                                widget=forms.PasswordInput(
+                                    attrs={'placeholder': 'Enter your password', 'class': 'form-control'}
+                                )
+                                )
+    password2 = forms.CharField(label='Confirm Password',
+                                widget=forms.PasswordInput(
+                                    attrs={'placeholder': 'Enter your password', 'class': 'form-control'}
+                                )
+                                )
+
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = User.objects.filter(email=email).exists()
+        if user:
+            raise ValidationError('This email address is already exists!')
+        return email
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = User.objects.filter(username=username).exists()
+        if user:
+            raise ValidationError('This username is already exists!')
+        return username
+
+    def clean(self):
+        cd = super().clean()
+        p1 = cd.get('password1')
+        p2 = cd.get('password2')
+        if p1 and p2 and p1 != p2:
+            raise ValidationError('Passwords is not match')
+
+
+class UserLoginForm(forms.Form):
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={'placeholder': 'Enter your Username', 'class': 'form-control'}
+    ))
+    password = forms.CharField(label='Password',
+                               widget=forms.PasswordInput(
+                                   attrs={'placeholder': 'Enter your password', 'class': 'form-control'}
+                               )
+                               )

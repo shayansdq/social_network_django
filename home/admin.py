9a3673e3f567983340f845b4928a6dcabecb864e
Shayan <shayansadeghi178@gmail.com>
2022-02-14 13:15:42 +0330

    customize user admin for comment model

--- home/admin.py
+++ home/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment, Vote
 
 
 # Register your models here.
@@ -11,3 +11,11 @@ class PostAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('body',)}
     raw_id_fields = ('user',)
 
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('user', 'post', 'created', 'is_reply')
+    raw_id_fields = ('user', 'post', 'reply')
+
+
+admin.site.register(Vote)

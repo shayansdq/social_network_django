343a81f121332d0797879edc1b31280771a73f46
Shayan <shayansadeghi178@gmail.com>
2022-02-14 14:51:39 +0330

    import post model

--- account/views.py
+++ account/views.py
@@ -5,7 +5,6 @@ from django.views import View
 from account.forms import UserRegistrationForm, UserLoginForm, EditUserForm
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
-from home.models import Post
 from django.contrib.auth import views as auth_views
 from django.urls import reverse_lazy
 from .models import Relation

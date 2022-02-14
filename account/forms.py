38a8d707b9495080138158bb678709083c3f8658
Shayan <shayansadeghi178@gmail.com>
2022-02-14 13:46:22 +0330

    import profile

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+from .models import Profile
 
 
 class UserRegistrationForm(forms.Form):
@@ -54,3 +55,11 @@ class UserLoginForm(forms.Form):
                                    attrs={'placeholder': 'Enter your password', 'class': 'form-control'}
                                )
                                )
+
+
+class EditUserForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = Profile
+        fields = ('age', 'bio')

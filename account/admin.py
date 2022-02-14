ad5767a71e31527ccffa7d15e19a7ea73082e5b1
Shayan <shayansadeghi178@gmail.com>
2022-02-14 13:15:27 +0330

    customize user admin for profile model

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Relation, Profile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 
 # Register your models here.
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+
+
+class ExtendedUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.register(Relation)
+
+admin.site.unregister(User)
+admin.site.register(User, ExtendedUserAdmin)

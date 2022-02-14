58c99f246ecef670e8fed9bb0a03ad3b115ca6e8
Shayan <shayansadeghi178@gmail.com>
2022-02-14 13:51:00 +0330

    config email options

--- social_network_django/settings.py
+++ social_network_django/settings.py
@@ -126,3 +126,12 @@ AUTHENTICATION_BACKENDS = [
 ]
 
 LOGIN_URL = '/account/login/'
+
+# Google account
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'shayansadeghi178@gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_PASSWORD = 'yvwmmuijqcwrhynz'
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = 'Shayan social Website'

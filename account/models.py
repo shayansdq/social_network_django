62ace59294a8b0a7dea700c6d3bb383f7aa2f4f7
Shayan <shayansadeghi178@gmail.com>
2022-02-14 13:47:32 +0330

    create relation and profile models

--- account/models.py
+++ account/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 # Create your models here.
+
+class Relation(models.Model):
+    from_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followers')
+    to_user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='followings')
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.from_user} following {self.to_user}"
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    age = models.PositiveSmallIntegerField(default=0)
+    bio = models.TextField(null=True, blank=True)
